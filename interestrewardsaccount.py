bb797da705d143a15766b25c1be889582c565335
Umer <umerfarhan47@gmail.com>
2023-12-12 15:27:02 +0400

    Removed an initialisation parameter

--- interestrewardsaccount.py
+++ interestrewardsaccount.py
@@ -1,10 +1,14 @@
 from parentbank import *
 
 class InterestRewardsAccount(BankAccount):
-    def __init__(self, name, balance, type):
-        super().__init__(self, name, balance, type)
 
     def deposit(self, amount):
         bonus = 1.05
         self.balance = self.balance + (amount * bonus)
+
+    def __str__(self):
+        return super().__str__() + f'Your account: {self.name}\n'\
+            f'Has balance: ${self.balance:.2f}\n'\
+                f'Account type: {self.type}\n'\
+                    f'Deposit has bonus of: {bonus}'
 
